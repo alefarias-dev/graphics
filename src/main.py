7edb304695fa5af3942d373b2d7f529e36a6c0c5
Alexandre <mail.alefarias@gmail.com>
2019-03-09 19:43:06 -0300

    Adds load modified image and hist functions

--- src/main.py
+++ src/main.py
@@ -2,26 +2,38 @@ import sys
 
 from image_processing.filters import clarear, escurecer
 from image_processing.data import histogram
-from util import img2Temp
+from util import img2Temp, imgClone
 
 from PySide2 import *
 from PySide2.QtWidgets import *
 from PySide2.QtCore import *
 from PySide2.QtGui import QPixmap
 
+FILENAMES = {
+    'original':'../.temp/original.png',
+    'modified': '../.temp/modified.png',
+    'hist_original': '../.temp/hist_original.png',
+    'hist_modified': '../.temp/hist_modified.png'
+}
+
+IMG_SCALE = (450, 300)
 
 class MainWindow(QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setWindowTitle("Computer Graphics")
+        self.originalFilename = ""
 
         # definicao das imagens
-        pixmap = QPixmap('../.temp/pardal.jpg')
-        pixmap = pixmap.scaled(450, 300)
+        pixmap = QPixmap(FILENAMES['original'])
+        pixmap = pixmap.scaled(*IMG_SCALE)
         self.imagemOriginalText = QLabel('Imagem original')
         self.imagemOriginal = QLabel()
         self.imagemOriginal.setPixmap(pixmap)
+
+        pixmap = QPixmap(FILENAMES['modified'])
+        pixmap = pixmap.scaled(*IMG_SCALE)
         self.imagemModificadaText = QLabel('Imagem modificada')
         self.imagemModificada = QLabel()
         self.imagemModificada.setPixmap(pixmap)
@@ -30,11 +42,17 @@ class MainWindow(QMainWindow):
         self.btnLoadImage = QPushButton('Carregar imagem...')
         self.btnLoadImage.clicked.connect(self.openImage)
         self.btnResetImage = QPushButton('Resetar imagem')
+        self.btnResetImage.clicked.connect(self.resetModifiedImage)
 
-        pixmap = QPixmap('../.temp/hist_original.png')
-        pixmap = pixmap.scaled(450, 300)
+        # original histogram
+        pixmap = QPixmap(FILENAMES['hist_original'])
+        pixmap = pixmap.scaled(*IMG_SCALE)
         self.histogramaOriginal = QLabel()
         self.histogramaOriginal.setPixmap(pixmap)
+
+        # modified histogram
+        pixmap = QPixmap(FILENAMES['hist_modified'])
+        pixmap = pixmap.scaled(*IMG_SCALE)
         self.histogramaModificado = QLabel()
         self.histogramaModificado.setPixmap(pixmap)
 
@@ -68,12 +86,13 @@ class MainWindow(QMainWindow):
         layout.addWidget(self.imagemModificada, 0, 1)
         layout.addWidget(self.btnResetImage, 1, 1)
         layout.addWidget(self.histogramaModificado, 2, 1)
+
+        # filtros:
         layout.addWidget(gpClarear, 3, 0)
         layout.addWidget(gpEscurecer, 3, 1)
 
         widget = QWidget()  # nossa widget principal
         widget.setLayout(layout)  # seta o layout a ser usado
-
         self.setCentralWidget(widget)
 
     def openImage(self):
@@ -81,11 +100,32 @@ class MainWindow(QMainWindow):
         dialog.setNameFilter('Images (*.png *.xpm *.jpg)')
         filename, _ = dialog.getOpenFileName()
         self.loadImage(filename)
-    
+
     def loadImage(self, filename):
-        new_filename = img2Temp(filename)
-        pixmap = QPixmap(new_filename).scaled(450, 300)
+        self.originalFilename = filename
+        new_filename, img = img2Temp(filename)
+        pixmap = QPixmap(new_filename).scaled(*IMG_SCALE)
         self.imagemOriginal.setPixmap(pixmap)
+        self.updateHistogram('hist_original', img)
+        self.resetModifiedImage()
+
+    def resetModifiedImage(self):
+        _, img = imgClone(self.originalFilename, 'modified.png')
+        pixmap = QPixmap(FILENAMES['modified']).scaled(*IMG_SCALE)
+        self.imagemModificada.setPixmap(pixmap)
+        self.updateHistogram('hist_modified', img)
+
+    def updateModifiedImage(self):
+        pass
+
+    def updateHistogram(self, which, img):
+        filename = FILENAMES[which]
+        histogram(img, filename)
+        pixmap = QPixmap(filename).scaled(*IMG_SCALE)
+        if which == 'hist_original':
+            self.histogramaOriginal.setPixmap(pixmap)
+            return
+        self.histogramaModificado.setPixmap(pixmap)
 
 
 if __name__ == "__main__":
--- src/util.py
+++ src/util.py
@@ -4,10 +4,17 @@ import cv2
 def img2Temp(filename):
     """ Recebe o endereco de uma imagem
     e salva a imagem na pasta temp do projeto
+    retorna endereco da imagem e imagem em RGB
     """
     img = cv2.imread(filename)
     filename = filename.split('/')[-1]
     extension = filename.split('.')[-1]
-    new_filename = f'../.temp/original.{extension}'
+    new_filename = f'../.temp/original.png'
     cv2.imwrite(new_filename, img)
-    return new_filename
+    return new_filename, cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+def imgClone(filename, new_filename):
+    img = cv2.imread(filename)
+    cv2.imwrite(f'../.temp/{new_filename}', img)
+    return new_filename, cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
