e0462ccf5a9913504b7c8549dbc8c56ea0ba0a9c
Alexandre <mail.alefarias@gmail.com>
2019-03-09 22:08:20 -0300

    Udates histogram style

--- src/image_processing/data.py
+++ src/image_processing/data.py
@@ -14,11 +14,7 @@ def histograma_bandas(img):
             r_hist[img[linha, coluna, 0]] += 1
             g_hist[img[linha, coluna, 1]] += 1
             b_hist[img[linha, coluna, 2]] += 1
-    return normalize(r_hist), normalize(g_hist), normalize(b_hist)
-
-
-def normalize(values):
-    return values
+    return r_hist, g_hist, b_hist
 
 
 def histogram(img, filename):
@@ -26,14 +22,15 @@ def histogram(img, filename):
     os subplots de histogramas para cada
     banda
     """
+    plt.style.use('bmh')
     r_hist, g_hist, b_hist = histograma_bandas(img)
     fig, axarr = plt.subplots(3, sharex=True, figsize=(4.26, 3.18))
     plt.subplots_adjust(hspace=.5)
     x = list(range(256))
-    axarr[0].bar(x, height=r_hist, color='red')
+    axarr[0].bar(x, height=r_hist, color='red', alpha=.64, width=1)
     axarr[0].set_title('Histograma Vermelho', fontsize=8)
-    axarr[1].bar(x, height=g_hist, color='green')
+    axarr[1].bar(x, height=g_hist, color='green', alpha=.64, width=1)
     axarr[1].set_title('Histograma Verde', fontsize=8)
-    axarr[2].bar(x, height=b_hist, color='blue')
+    axarr[2].bar(x, height=b_hist, color='blue', alpha=.64, width=1)
     axarr[2].set_title('Histograma Azul', fontsize=8)
     plt.savefig(filename)
