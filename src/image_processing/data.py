c02dfe6bdaf620eec510029296c4c17dac04d4eb
Alexandre Farias <mail.alefarias@gmail.com>
2019-03-11 12:46:39 -0300

    Adds some comments to main and data files

--- src/image_processing/data.py
+++ src/image_processing/data.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 
 
 def histograma_bandas(img):
-    """ recebe uma imagem e um inteiro especificando uma banda,
-    retorna o vetor para montar o histograma daquela banda
+    """ recebe uma imagem RGB (numpy.array), retorna os vetores 
+    para montar o histograma das bandas
     """
     linhas, colunas, _ = img.shape
     r_hist = [0] * 256
@@ -17,20 +17,19 @@ def histograma_bandas(img):
     return r_hist, g_hist, b_hist
 
 
-def histogram(img, filename):
-    """ recebe uma img RGB e retorna
-    os subplots de histogramas para cada
-    banda
+def histogram(img, filename, show_log=False):
+    """ recebe uma imagem RGB (numpy.array) e salva o plot dos
+    histogramas no filename indicado
     """
     plt.style.use('bmh')
     r_hist, g_hist, b_hist = histograma_bandas(img)
     fig, axarr = plt.subplots(3, sharex=True, figsize=(4.26, 3.18))
     plt.subplots_adjust(hspace=.5)
     x = list(range(256))
-    axarr[0].bar(x, height=r_hist, color='red', alpha=.64, width=1)
+    axarr[0].bar(x, height=r_hist, color='red', alpha=.64, width=1, log=show_log)
     axarr[0].set_title('Histograma Vermelho', fontsize=8)
-    axarr[1].bar(x, height=g_hist, color='green', alpha=.64, width=1)
+    axarr[1].bar(x, height=g_hist, color='green', alpha=.64, width=1, log=show_log)
     axarr[1].set_title('Histograma Verde', fontsize=8)
-    axarr[2].bar(x, height=b_hist, color='blue', alpha=.64, width=1)
+    axarr[2].bar(x, height=b_hist, color='blue', alpha=.64, width=1, log=show_log)
     axarr[2].set_title('Histograma Azul', fontsize=8)
     plt.savefig(filename)
--- src/main.py
+++ src/main.py
@@ -9,6 +9,9 @@ from PySide2.QtWidgets import *
 from PySide2.QtCore import *
 from PySide2.QtGui import QPixmap
 
+
+# arquivos utilizados para salvar imagens e histogramas
+IMG_SCALE = (426, 326)
 FILENAMES = {
     'original':'../.temp/original.png',
     'modified': '../.temp/modified.png',
@@ -16,7 +19,6 @@ FILENAMES = {
     'hist_modified': '../.temp/hist_modified.png'
 }
 
-IMG_SCALE = (426, 326)
 
 class MainWindow(QMainWindow):
 
@@ -25,38 +27,39 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Computer Graphics")
         self.originalFilename = ""
 
-        # definicao das imagens
+        # Elementos para imagem original
         pixmap = QPixmap(FILENAMES['original'])
         pixmap = pixmap.scaled(*IMG_SCALE)
         self.imagemOriginalText = QLabel('Imagem original')
         self.imagemOriginal = QLabel()
         self.imagemOriginal.setPixmap(pixmap)
 
+        # Elementos para imagem modificada
         pixmap = QPixmap(FILENAMES['modified'])
         pixmap = pixmap.scaled(*IMG_SCALE)
         self.imagemModificadaText = QLabel('Imagem modificada')
         self.imagemModificada = QLabel()
         self.imagemModificada.setPixmap(pixmap)
 
-        # load and reset buttons
+        # Botoes para carregar e resetar imagem
         self.btnLoadImage = QPushButton('Carregar imagem...')
         self.btnLoadImage.clicked.connect(self.openImage)
         self.btnResetImage = QPushButton('Resetar imagem')
         self.btnResetImage.clicked.connect(self.resetModifiedImage)
 
-        # original histogram
+        # Elementos para o histograma original
         pixmap = QPixmap(FILENAMES['hist_original'])
         pixmap = pixmap.scaled(*IMG_SCALE)
         self.histogramaOriginal = QLabel()
         self.histogramaOriginal.setPixmap(pixmap)
 
-        # modified histogram
+        # Elementos para o histograma modificado
         pixmap = QPixmap(FILENAMES['hist_modified'])
         pixmap = pixmap.scaled(*IMG_SCALE)
         self.histogramaModificado = QLabel()
         self.histogramaModificado.setPixmap(pixmap)
 
-        # cria group boxes
+        # Groupbox do filtro clarear
         gpClarear = QGroupBox("Filtro Clarear")
         self.escalarClarearText = QLabel("Escalar:")
         self.escalarClarear = QLineEdit()
@@ -69,6 +72,7 @@ class MainWindow(QMainWindow):
         vboxClarear.addStretch(1)
         gpClarear.setLayout(vboxClarear)
 
+        # Groupbox do filtro escurecer
         gpEscurecer = QGroupBox("Filtro Escurecer")
         self.escalarEscurecerText = QLabel("Escalar:")
         self.escalarEscurecer = QLineEdit()
@@ -81,6 +85,7 @@ class MainWindow(QMainWindow):
         vboxEscurecer.addStretch(1)
         gpEscurecer.setLayout(vboxEscurecer)
 
+        # Definicoes do layout da janela
         layout = QGridLayout()
         layout.addWidget(self.imagemOriginal, 0, 0)
         layout.addWidget(self.btnLoadImage, 1, 0)
@@ -89,15 +94,18 @@ class MainWindow(QMainWindow):
         layout.addWidget(self.btnResetImage, 1, 1)
         layout.addWidget(self.histogramaModificado, 2, 1)
 
-        # filtros:
+        # Adiciona groupbox de filtros na janela
         layout.addWidget(gpClarear, 3, 0)
         layout.addWidget(gpEscurecer, 3, 1)
 
-        widget = QWidget()  # nossa widget principal
-        widget.setLayout(layout)  # seta o layout a ser usado
+        widget = QWidget()  # Widget principal
+        widget.setLayout(layout)  # define o layout a ser usado
         self.setCentralWidget(widget)
 
     def clarearWrapper(self):
+        """ Wrapper para o filtro clarear que
+        chama a funcao com o input do usuario
+        """
         escalar = self.escalarClarear.text()
         img = readImage(FILENAMES['modified'])
         modified = clarear(img, int(escalar))
@@ -105,6 +113,9 @@ class MainWindow(QMainWindow):
         self.messageBox('Filtro aplicado!')
 
     def escurecerWrapper(self):
+        """ Wrapper para o filtro escurecer que
+        chama a funcao com o input do usuario
+        """
         escalar = self.escalarEscurecer.text()
         img = readImage(FILENAMES['modified'])
         modified = escurecer(img, int(escalar))
@@ -112,12 +123,18 @@ class MainWindow(QMainWindow):
         self.messageBox('Filtro aplicado!')
 
     def openImage(self):
+        """ Funcao que abre uma caixa de dialogo
+        para selecao de imagem
+        """
         dialog = QFileDialog()
         dialog.setNameFilter('Images (*.png *.xpm *.jpg)')
         filename, _ = dialog.getOpenFileName()
         self.loadImage(filename)
 
     def loadImage(self, filename):
+        """ Funcao para carregar a imagem no 
+        pixmap da imagem original
+        """
         self.originalFilename = filename
         new_filename, img = img2Temp(filename)
         pixmap = QPixmap(new_filename).scaled(*IMG_SCALE)
@@ -127,18 +144,28 @@ class MainWindow(QMainWindow):
         self.messageBox('Imagem carregada!')
 
     def updateModifiedImage(self, img):
+        """ Atualiza a imagem modificada para a imagem
+        passada pelo parametro img (numpy.array)
+        """
         saveImage(FILENAMES['modified'], img)
         pixmap = QPixmap(FILENAMES['modified']).scaled(*IMG_SCALE)
         self.imagemModificada.setPixmap(pixmap)
         self.updateHistogram('hist_modified', img)
 
     def resetModifiedImage(self):
+        """ Desfaz as tranformacoes aplicadas na imagem modificada,
+        retornando para o estado da imagem original
+        """
        self.updateModifiedImage(readImage(FILENAMES['original']))
        self.messageBox('Imagem restaurada!')
 
     def updateHistogram(self, which, img):
+        """ Atualiza o histograma de uma imagem,
+        o parametro which indica se eh da imagem original
+        ou da imagem modificada
+        """
         filename = FILENAMES[which]
-        histogram(img, filename)
+        histogram(img, filename, show_log=True)
         pixmap = QPixmap(filename).scaled(*IMG_SCALE)
         if which == 'hist_original':
             self.histogramaOriginal.setPixmap(pixmap)
@@ -146,6 +173,9 @@ class MainWindow(QMainWindow):
         self.histogramaModificado.setPixmap(pixmap)
 
     def messageBox(self, message):
+        """ Exibe uma caixa de mensagens para o usuario
+        com a mensagem passada por parametro
+        """
         msgBox = QMessageBox().information(self, "Informativo", message)
 
 
